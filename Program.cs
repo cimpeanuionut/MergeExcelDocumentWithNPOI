48f18b728341f390ba9b24d162e46793f2e7f317
cimpeanuionut <43180388+cimpeanuionut@users.noreply.github.com>
2018-10-25 21:35:09 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using System.Data;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+namespace NPOI
+{
+    class Program
+    {
+        
+		// dynamic pah construction
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return System.IO.Path.GetDirectoryName(path);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable();
+            string[] files = new string[] { @"your path" };
+           
+            for (int i = 0; i < files.Length; i++)
+            {
+				// use just one of this void 
+                MergeDataXLS(files[i], dt);
+				// OR
+				MergeDataXLSX(files[i], dt);
+                                
+            }
+            ExportEasy(dt, @"path Export" );
+        
+		
+		//use this void for .xls file
+        private static void MergeDataXLS(string path, DataTable dt)
+        {
+            HSSFWorkbook workbook;
+            using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                workbook = new HSSFWorkbook(file);
+            }
+            HSSFSheet sheet = (HSSFSheet)workbook.GetSheetAt(0);
+            HSSFRow headerRow = (HSSFRow)sheet.GetRow(0);
+            int cellCount = headerRow.LastCellNum;
+            if (dt.Rows.Count == 0)
+            {
+                for (int i = headerRow.FirstCellNum; i < cellCount; i++)
+                {
+                    DataColumn column = new DataColumn(headerRow.GetCell(i).StringCellValue);
+                    dt.Columns.Add(column);
+                }
+            }
+            else
+            {
+            }
+
+            int rowCount = sheet.LastRowNum + 1;
+            for (int i = (sheet.FirstRowNum + 1); i < rowCount; i++)
+            {
+                HSSFRow row = (HSSFRow)sheet.GetRow(i);
+                DataRow dataRow = dt.NewRow();
+                for (int j = row.FirstCellNum; j < cellCount; j++)
+                {
+                    if (row.GetCell(j) != null)
+                        dataRow[j] = row.GetCell(j).ToString();
+                }
+                dt.Rows.Add(dataRow);
+            }
+            workbook = null;
+            sheet = null;
+
+        }
+
+		// use this void for .xlsx file
+        private static void MergeDataXLSX(string path, DataTable dt)
+        {
+            XSSFWorkbook workbook;
+            using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                workbook = new XSSFWorkbook(file);
+            }
+            XSSFSheet sheet = (XSSFSheet)workbook.GetSheetAt(0);
+            XSSFRow headerRow = (XSSFRow)sheet.GetRow(0);
+            int cellCount = headerRow.LastCellNum;
+            if (dt.Rows.Count == 0)
+            {
+                for (int i = headerRow.FirstCellNum; i < cellCount; i++)
+                {
+                    DataColumn column = new DataColumn(headerRow.GetCell(i).StringCellValue);
+                    dt.Columns.Add(column);
+                }
+            }
+            else
+            {
+            }
+
+            int rowCount = sheet.LastRowNum + 1;
+            for (int i = (sheet.FirstRowNum + 1); i < rowCount; i++)
+            {
+                XSSFRow row = (XSSFRow)sheet.GetRow(i);
+                DataRow dataRow = dt.NewRow();
+                for (int j = row.FirstCellNum; j < cellCount; j++)
+                {
+                    if (row.GetCell(j) != null)
+                        dataRow[j] = row.GetCell(j).ToString();
+                }
+                dt.Rows.Add(dataRow);
+            }
+            workbook = null;
+            sheet = null;
+
+        }
+
+
+
+        public static void ExportEasy(DataTable dtSource, string strFileName)
+        {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = (HSSFSheet)workbook.CreateSheet();
+            HSSFRow dataRow = (HSSFRow)sheet.CreateRow(0);
+            foreach (DataColumn column in dtSource.Columns)
+            {
+                dataRow.CreateCell(column.Ordinal).SetCellValue(column.ColumnName);
+            }
+            for (int i = 0; i < dtSource.Rows.Count; i++)
+            {
+                dataRow = (HSSFRow)sheet.CreateRow(i + 1);
+                for (int j = 0; j < dtSource.Columns.Count; j++)
+                {
+                    dataRow.CreateCell(j).SetCellValue(dtSource.Rows[i][j].ToString());
+                }
+            }
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(strFileName, FileMode.Create, FileAccess.Write))
+                {
+                    workbook.Write(fs);
+                }
+            }
+        }
+    }
+}
